2ce279d95ed84e0af37da4b2f92b8f29def2f88a
taro2489 <laugh2489@gmail.com>
2025-08-12 22:58:40 +0900

    kadai_mdn_jsのアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+//現在の時刻の取得
+const today = new Date();
+//年の取得
+const year = today.getFullYear();
+//月の取得
+const month = today.getMonth();
+//日の取得
+const day = today.getDate();
+//年月日の表示
+console.log(year+"年"+month+"月"+day+"日")
